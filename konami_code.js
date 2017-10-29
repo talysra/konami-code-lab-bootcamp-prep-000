7d131e1c0f4c513dc904a365d425db738c09aa91
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-29 18:05:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,18 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(e)) {
-    const key
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Konami Code entered!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   }
 }
 
