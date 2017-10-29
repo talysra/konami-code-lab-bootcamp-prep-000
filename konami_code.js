efe6e5fd2e76c32269c789d94043b3c151d53626
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-29 17:56:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  function keyDownAlert {
     const keyboardEvent = document.querySelector('input')
-    keyboardEvent.addEventListener('keydown', function(event) {
-      alert("Konami Code!");
+      keyboardEvent.addEventListener('keydown', function(e) {
+        if (e.which === code) {
+          alert("Konami Code!");
+      }
     }
   }
-
-}
 
 init();
